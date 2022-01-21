1080e0eef5cfd30023ba7d8ca104e762579c9df6
BrendonsDad <orraubury@gmail.com>
2022-01-20 17:45:00 -0700

    Finished the high low game with two classes

--- hilogame.py
+++ hilogame.py
@@ -1,4 +1,57 @@
+import random
 
+class Player:
+    def __init__(self):
+        self.score = 300
+    
+    
+class Card:
+    def __init__(self):
+        self.cardnumber = random.randint(1, 14)
+
+def main():
+    player = Player()
+    player_score = player.score
+
+    play = ''
+    while play != 'done':
+        print()
+        card1 = Card()
+        card1num = card1.cardnumber
+        print(f'The card is: {card1num}')
+        hilochoice = input('Higher or lower? [h/l] ')
+        card2 = Card()
+        card2num = card2.cardnumber
+        print(f'Next card: {card2num}')
+        if card1num > card2num:
+            if hilochoice.lower() == 'l':
+                player_score = player_score + 100
+            else:
+                player_score = player_score - 75
+        
+        elif card1num < card2num:
+            if hilochoice.lower() == 'h':
+                player_score = player_score + 100
+            else:
+                player_score = player_score - 75
+        
+        else:
+            player_score = player_score + 0
+
+        print(f'Your score is: {player_score}')
+
+        if player_score > 0:
+            play_again = input('Play again? [y/n] ')
+            if play_again.lower() == 'y':
+                bob = 'bob'
+            else:
+                play = 'done'
+        else:
+            play = 'done'
+        
+
+    
+main()
 
 # import random
 # player_score = 300
