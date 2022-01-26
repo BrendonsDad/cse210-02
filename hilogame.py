822c62f7f63c3199dd0bb92d016762d94f236fe1
BrendonsDad <orraubury@gmail.com>
2022-01-26 15:02:32 -0700

    made card 1 be card 2

--- hilogame.py
+++ hilogame.py
@@ -15,10 +15,11 @@ def main():
 
     player_luck = 0
     play = ''
+    card1 = Card()
+    card1num = card1.cardnumber
     while play != 'done':
         print()
-        card1 = Card()
-        card1num = card1.cardnumber
+        
         print(f'The card is: {card1num}')
         hilochoice = input('Higher or lower? [h/l] ')
         card2 = Card()
@@ -54,7 +55,7 @@ def main():
         if player_score > 0:
             play_again = input('Play again? [y/n] ')
             if play_again.lower() == 'y':
-                bob = 'bob'
+                card1num = card2num
             else:
                 play = 'done'
         else:
@@ -63,39 +64,3 @@ def main():
 
     
 main()
-
-# import random
-# player_score = 300
-# play = ''
-# while play != 'done':
-#     print()
-#     cardnum1 = random.randint(1,14)
-#     print(f'The card is: {cardnum1}')
-#     playernum = input('Higher or lower? [h/l] ')
-#     cardnum2 = random.randint(1, 14)
-#     print(f'Next card was: {cardnum2}')
-#     if cardnum1 > cardnum2:
-#         if playernum.lower() == 'l':
-#             player_score = player_score + 100
-#         else:
-#             player_score = player_score - 75
-    
-#     elif cardnum1 < cardnum2:
-#         if playernum.lower() == 'h':
-#             player_score = player_score + 100
-#         else:
-#             player_score = player_score - 75
-    
-#     else:
-#         player_score = player_score + 0
-
-#     print(f'Your score is: {player_score}')
-
-#     if player_score > 0:
-#         play_again = input('Play again? [y/n] ')
-#         if play_again.lower() == 'y':
-#             bob = 'bob'
-#         else:
-#             play = 'done'
-#     else:
-#         play = 'done'
