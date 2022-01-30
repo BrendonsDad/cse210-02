6457194b5447f022d8c1184bcd333f12e2eb7dad
BrendonsDad <orraubury@gmail.com>
2022-01-29 19:12:05 -0700

    Added the card icons:) Ellas idea

--- card.py
+++ card.py
@@ -1,6 +1,32 @@
 import random
+
 class Card:
     def __init__(self):
-        self.cardnumber = random.randint(1, 14)
+        self.card1num = random.randint(1, 14)
+        self.card1num_card_suit = '♡'
+        self.card2num = random.randint(1, 13)
+        self.card2num_card_suit = '♢'
+    def draw_card(self):
+        # Generate a ramdom number bwteen 1 and 14 for previous card
+        self.card2num = self.card1num
+        self.card2num_card_suit = self.card1num_card_suit
+        self.card1num = random.randint(1, 14)
+        self.rand_card_suit()
+    def rand_card_suit(self):
+        # Generate a random suit for each card
+        suit_temp = random.randint(1, 4)
+        if suit_temp == 1:
+            self.card1num_card_suit = '♡'
+        elif suit_temp == 2:
+            self.card1num_card_suit = '♢'
+        elif suit_temp == 3:
+            self.card1num_card_suit = '♤'
+        else:
+            self.card1num_card_suit = '♧'
+
+
+# class Card:
+#     def __init__(self):
+#         self.cardnumber = random.randint(1, 14)
 
 
--- hilogame.py
+++ hilogame.py
@@ -1,37 +1,30 @@
 from card import Card
 from player import Player
 
+card = Card()
 class Director:
-
     def __init__(self):
-        
         self.player_luck = 0
         self.play = ''
-
     def playgame(self):
-
         ## Makes a new player so we can keep their score
         player = Player()
         player_score = player.score
-
         ## Calls the card class so we can generate the 
         ## first card
         card1 = Card()
-        card1num = card1.cardnumber
-        ## Calls the Director class so we can have some
-        ## variables needed to start the game
+        card1num = card1.card1num
         gamespecs = Director()
         player_luck = gamespecs.player_luck
         play = gamespecs.play
-
         def generate_scores(card1num, card2num, hilochoice, player_score, player_luck):
             ## This function will check their answers, and
-            ## see if they guessed correctly. If they did,
-            ## it will reward them with 100 points. If not, 
-            ## they lose 75 points. Also include code for 
-            ## the easter egg which will tallies each time
-            ## they guess wrong but resets when they guess 
-            ## right
+             ## see if they guessed correctly. If they did,
+             ## it will reward them with 100 points. If not, 
+             ## they lose 75 points. Also include code for 
+             ## the easter egg which will tallies each time
+             ## they guess wrong but resets when they guess 
+             ## right
             if card1num > card2num:
                 if hilochoice.lower() == 'l':
                     player_score = player_score + 100
@@ -39,8 +32,6 @@ class Director:
                 else:
                     player_score = player_score - 75
                     player_luck = player_luck + 1
-
-            
             elif card1num < card2num:
                 if hilochoice.lower() == 'h':
                     player_score = player_score + 100
@@ -48,40 +39,30 @@ class Director:
                 else:
                     player_score = player_score - 75
                     player_luck = player_luck +1
-
-            
             else:
                 player_score = player_score + 0
-
-            ## Returns a list of all needed answers
             cardsandscores = [card1num, card2num, hilochoice, player_score, player_luck]
+            ## Returns a list of all needed answers
             return cardsandscores
-
-
-
         while play != 'done':
             print()
-            
-            print(f'The card is: {card1num}')
+            print(f'The card is: {card.card1num_card_suit}{card1num}')
             hilochoice = input('Higher or lower? [h/l] ')
             card2 = Card()
-            card2num = card2.cardnumber
-            print(f'Next card: {card2num}')
-
+            card.draw_card()
+            card2num = card2.card2num
+            print(f'Next card: {card.card1num_card_suit}{card2num}')
             cardsandscores = generate_scores(card1num, card2num, hilochoice, player_score, player_luck)
             card1num = cardsandscores[0]
             card2num = cardsandscores[1]
             hilochoice = cardsandscores[2]
             player_score = cardsandscores[3]
             player_luck = cardsandscores[4]
-            
-
             print(f'Your score is: {player_score}')
             if player_luck == 3:
                 print('You have horrible luck...')
             else:
                 bob = 'bob'
-
             if player_score > 0:
                 play_again = input('Play again? [y/n] ')
                 if play_again.lower() == 'y':
@@ -90,6 +71,101 @@ class Director:
                     play = 'done'
             else:
                 play = 'done'
+
+
+
+
+
+
+# class Director:
+
+#     def __init__(self):
+        
+#         self.player_luck = 0
+#         self.play = ''
+
+#     def playgame(self):
+
+#         ## Makes a new player so we can keep their score
+#         player = Player()
+#         player_score = player.score
+
+#         ## Calls the card class so we can generate the 
+#         ## first card
+#         card1 = Card()
+#         card1num = card1.card1num
+#         ## Calls the Director class so we can have some
+#         ## variables needed to start the game
+#         gamespecs = Director()
+#         player_luck = gamespecs.player_luck
+#         play = gamespecs.play
+
+#         def generate_scores(card1num, card2num, hilochoice, player_score, player_luck):
+#             ## This function will check their answers, and
+#             ## see if they guessed correctly. If they did,
+#             ## it will reward them with 100 points. If not, 
+#             ## they lose 75 points. Also include code for 
+#             ## the easter egg which will tallies each time
+#             ## they guess wrong but resets when they guess 
+#             ## right
+#             if card1num > card2num:
+#                 if hilochoice.lower() == 'l':
+#                     player_score = player_score + 100
+#                     player_luck = 0
+#                 else:
+#                     player_score = player_score - 75
+#                     player_luck = player_luck + 1
+
+            
+#             elif card1num < card2num:
+#                 if hilochoice.lower() == 'h':
+#                     player_score = player_score + 100
+#                     player_luck = 0
+#                 else:
+#                     player_score = player_score - 75
+#                     player_luck = player_luck +1
+
+            
+#             else:
+#                 player_score = player_score + 0
+
+#             ## Returns a list of all needed answers
+#             cardsandscores = [card1num, card2num, hilochoice, player_score, player_luck]
+#             return cardsandscores
+
+
+
+#         while play != 'done':
+#             print()
+            
+#             print(f'The card is: {card1num}')
+#             hilochoice = input('Higher or lower? [h/l] ')
+#             card2 = Card()
+#             card2num = card2.cardnumber
+#             print(f'Next card: {card2num}')
+
+#             cardsandscores = generate_scores(card1num, card2num, hilochoice, player_score, player_luck)
+#             card1num = cardsandscores[0]
+#             card2num = cardsandscores[1]
+#             hilochoice = cardsandscores[2]
+#             player_score = cardsandscores[3]
+#             player_luck = cardsandscores[4]
+            
+
+#             print(f'Your score is: {player_score}')
+#             if player_luck == 3:
+#                 print('You have horrible luck...')
+#             else:
+#                 bob = 'bob'
+
+#             if player_score > 0:
+#                 play_again = input('Play again? [y/n] ')
+#                 if play_again.lower() == 'y':
+#                     card1num = card2num
+#                 else:
+#                     play = 'done'
+#             else:
+#                 play = 'done'
         
 
     
